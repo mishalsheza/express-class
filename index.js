9aa4f930a56793046e45f73129150402db4c3d0c
shezamishal19 <sheza.mishal@mail.com>
2024-07-03 09:50:16 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+let courses = [
+    {id:1,name:"java"},
+    {id:2,name:"c++"},
+    {id:3,name:"python"},
+];
+
+app.get('/courses',(req,res) =>{
+    res.json(courses);
+
+});
+
+app.listen(3000,() =>{
+    console.log("server started");
+})
